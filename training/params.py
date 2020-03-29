c28c95a58ca1ff99a42dc8e83b91d6b91089a070
lianxiaorui <347467045@qq.com>
2020-03-29 13:19:01 +0000

    first commit

---
+++ training/params.py
@@ -0,0 +1,34 @@
+TRAINING_PARAMS = \
+{
+    "model_params": {
+        "backbone_name": "darknet_53",
+        "backbone_pretrained": "../weights/darknet53_weights_pytorch.pth", #  set empty to disable
+    },
+    "yolo": {
+        "anchors": [[[116, 90], [156, 198], [373, 326]],
+                    [[30, 61], [62, 45], [59, 119]],
+                    [[10, 13], [16, 30], [33, 23]]],
+        "classes": 2},
+    "lr": {
+        "backbone_lr": 0.001,
+        "other_lr": 0.01,
+        "freeze_backbone": False,  
+        "decay_gamma": 0.1,
+        "decay_step": 20,           #  decay lr in every ? epochs
+    },
+    "optimizer": {
+        "type": "sgd",
+        "weight_decay": 4e-05,
+    },
+    "batch_size": 4,
+    "train_path": "../data/path_list.txt", 
+    "epochs": 100,
+    "img_h": 416,
+    "img_w": 416,
+    "parallels": [0,1,2,3],                        
+    "working_dir": "checkpoints",              #  replace with your working dir
+    "pretrain_snapshot": "",                        #  load checkpoint
+    "evaluate_type": "", 
+    "try": 0,
+    "export_onnx": False,
+}
